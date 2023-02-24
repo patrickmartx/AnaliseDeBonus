f9615566622e6c115b93226f2ea9632686649591
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-02-24 10:48:45 -0300

    Recebendo os vendedores que venderam mais de 55000 e printando na tela informações sobre

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ import pandas as pd
 lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
 
 for mes in lista_meses:
-    print(mes)
     tabela_vendas = pd.read_excel(f'{mes}.xlsx')
-    print(tabela_vendas)
-
 # Para cada arquivo:
-#   Verificar se algum valor na coluna de vendas é maior que 55.000
+    if (tabela_vendas['Vendas'] > 55000).any():   # Verificar se algum valor na coluna de vendas é maior que 55.000
+        vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
+        print(f'No mês de {mes.capitalize()}, alguém bateu a meta.\nVendedor: {vendedor}\nVendas: {vendas}')
+        # Se for maior do que 55.000 -> Envia um SMS com o Nome, o mês e as vendas do vendedor
 
-# Se for maior do que 55.000 -> Envia um SMS com o Nome, o mês e as vendas do vendedor
+
 
 # Caso não seja maior do que 55.000, não quero fazer nada
