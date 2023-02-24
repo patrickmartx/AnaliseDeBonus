825e75234fdfa91a516d6879380c2d8fd7d41d9a
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-02-23 22:28:46 -0300

    pandas importado e lista de meses criada

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #Bibliotecas instaladas: pandas, openpyxl, twilio
+import pandas as pd
+
 # Passo a passo de solução
 
 # Abrir os 6 arquivos em Excel
+lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
+
+tabela_vendas = pd.read_excel('janeiro.xlsx')
 
 
 # Para cada arquivo:
