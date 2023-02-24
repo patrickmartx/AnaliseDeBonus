8ca2970857fa35a7d5681a5fce207c9f59d407c7
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-02-23 22:07:17 -0300

    Comentários a respeito do que vai ser feito

---
+++ main.py
@@ -0,0 +1,10 @@
+# Passo a passo de solução
+
+# Abrir os 6 arquivos em Excel
+
+# Para cada arquivo:
+#   Verificar se algum valor na coluna de vendas é maior que 55.000
+
+# Se for maior do que 55.000 -> Envia um SMS com o Nome, o mês e as vendas do vendedor
+
+# Caso não seja maior do que 55.000, não quero fazer nada
